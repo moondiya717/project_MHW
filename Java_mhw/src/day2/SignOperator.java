d40cbfb337cacaabea8c06462cb74c5d5ee0b20e
moondiya717 <83192162+moondiya717@users.noreply.github.com>
2021-04-27 17:45:31 +0900

    자바2일차: 부호연산자 예제

---
+++ Java_mhw/src/day2/SignOperator.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class SignOperator {
+
+	public static void main(String[] args) {
+		/*연산자의 종류
+		 * 1. 단항 연산자 2. 이항연산자 3. 삼항 연산자
+		 * 항 : 식을 계산하는데 필요한 수의 갯수
+		 * 단항연산자? 
+		 * a) 부호연산자 : -
+		 * b) 증감 연산자 : ++, -- 
+		 * */
+		
+		// 부호 연산자 예제
+		int num1 = 10;
+		System.out.println("-num1 = " + (-num1));
+		int num2 = -20;
+		System.out.println("-num2 =" + (-num2));
+		
+	}
+
+}
