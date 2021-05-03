fc2dfb036f02d0022f06ac5af19b3c69b894d92d
moondiya717 <83192162+moondiya717@users.noreply.github.com>
2021-05-03 17:35:59 +0900

    자바6일차: Class예제

---
+++ Java_mhw/src/day6/ClassEx1.java
@@ -0,0 +1,31 @@
+package day6;
+
+public class ClassEx1 {
+
+	public static void main(String[] args) {
+		// 교재 224-226의 필드선언
+
+		int x=1;	//int x; 로해놓고 출력하면 값이 안뜨는데 이유는 일반변수는 초기값이 자동으로 초기화 안됨.
+		Point1 point = new Point1();
+		System.out.println("main의 x: "+ x);
+		System.out.println("point의 x: "+ point.x);
+		point.x=3;
+		System.out.println("point의 x: " + point.x);
+		Point1 point2 = new Point1(1,2);
+		System.out.println("point2의 x: " + point2.x);
+
+	}
+}
+
+//2차원 좌표 평면의 점을 나타내는 클래스 : Point1
+//x좌표, y좌표 => 정수
+class Point1{
+	int x;	//멤버 변수는 초기값이 자동으로 초기값이 초기화
+	int y;
+	public Point1() {} //이라는 코드가 자동으로 만들어져서 객체를 실행할때 생성되야돼. //자동으로만들어지지만 안에 실행코드는 없어서 실행시켜도 뭐 안나옴. //클래스 명과 동일한 이름의 이 라인이 있으면 생성자라고 생각하면 됨.	
+	public Point1(int x1, int y1){
+		x=x1;
+		y=y1;
+	}
+}						
+
