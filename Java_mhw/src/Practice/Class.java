91bc88536ab2d11640d30096c461e82e527330cc
moondiya717 <83192162+moondiya717@users.noreply.github.com>
2021-05-04 17:51:23 +0900

    Update Class.java

--- Java_mhw/src/Practice/Class.java
+++ Java_mhw/src/Practice/Class.java
@@ -3,29 +3,25 @@ package Practice;
 public class Class {
 
 	public static void main(String[] args) {	//이 라인 밑으로는 모두 메인
-		//  교재 224-226의 필드선언
-		
-		int x=9;
-		System.out.println("main의 x의 값: " + x); 		//에러 발생: 일반변수 x는 자동으로 값이 초기화가 안되기때문에 변수에 값을 넣지 않으면(초기화) 출력안됨. 
-		
-		
-
 		
 	}
 
 }
-
-//2차원 좌표 평면의 점을 나타내는 클래스 : Point1
-//x좌표, y좌표 => 정수
-
-/* 	public class 클래스이름{				//public은 파일이름과 동일한 이름의 클래스 선언에만 붙임, 아니면 컴파일에러 남
- * 	}
- * */
- 
-//Point1 이름을 가진 클래스를 만들어보자
-class Point1{		//클래스 선언
+class Point1{		
 	
 
+class point2{
+	
+	
+	point2 point2;
+	point2 = new point2();
+	
+	
+	point2 point2 =new point2();
+	
+			
 }
+
+
 
 }
