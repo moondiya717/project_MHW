16e621a0e7ce13b6d4fbe64ad312623af74f382d
moondiya717 <moondiya717@gmail.com>
2021-05-16 23:05:14 +0900

    update

--- Java_mhw/src/HW/day14_Library.java
+++ Java_mhw/src/HW/day14_Library.java
@@ -4,23 +4,26 @@ public class day14_Library {
 
 //도서관리 프로그램을 만드려고 한다. 이 때 필요한 클래스는 무엇있는가?
 /** 1. Item - 항목
- *  2. LibraryProgram - 메소드
+ *  2. LibraryProgram - 기능 및 출력 메소드
  *  3. LibraryTest - 프로그램 구현
  *  * **/
 	
 
 //수강생 관리프로그램을 만드려고 한다. 이 때 필요한 클래스는 무엇인가?
 /** 1. Item - 항목
- *  2. StudentManagement - 메소드
+ *  2. StudentManagement - 기능 및 출력 메소드
  *  3. ManagementTest - 프로그램 구현
  *  * **/
 	
 //쇼핑몰을 운영하려고 한다. 이 때 필요한 클래스는 무엇인가?
 	
-
+	/** 1. Item - 항목
+	 *  2. ShoppingFunction - 기능 및 출력 메소드
+	 *  3. MallTest - 프로그램 구현
+	 *  * **/
 
 	
-	
+	//나는 클래스 제일 적게쓰고시펑
 	
 //자바는 객체지향 프로그램이기때문에 클래스가 겁나 중요하니깐 많이많이 연습하세요 ^____________^*
 	
