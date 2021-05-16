ab63e2e9fb6995abeed282384d71df4529d1d5c1
moondiya717 <moondiya717@gmail.com>
2021-05-16 23:03:01 +0900

    update

--- Java_mhw/src/HW/day14_Library.java
+++ Java_mhw/src/HW/day14_Library.java
@@ -3,10 +3,17 @@ package HW;
 public class day14_Library {
 
 //도서관리 프로그램을 만드려고 한다. 이 때 필요한 클래스는 무엇있는가?
+/** 1. Item - 항목
+ *  2. LibraryProgram - 메소드
+ *  3. LibraryTest - 프로그램 구현
+ *  * **/
 	
 
 //수강생 관리프로그램을 만드려고 한다. 이 때 필요한 클래스는 무엇인가?
-	
+/** 1. Item - 항목
+ *  2. StudentManagement - 메소드
+ *  3. ManagementTest - 프로그램 구현
+ *  * **/
 	
 //쇼핑몰을 운영하려고 한다. 이 때 필요한 클래스는 무엇인가?
 	
