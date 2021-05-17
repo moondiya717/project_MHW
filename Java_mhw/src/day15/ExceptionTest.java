617dbe16cd026485cdce98163f812483336981a6
moondiya717 <83192162+moondiya717@users.noreply.github.com>
2021-05-17 17:30:39 +0900

    자바15일차: 예외(처리)예제

---
+++ Java_mhw/src/day15/ExceptionTest.java
@@ -0,0 +1,34 @@
+package day15;
+
+import java.util.Scanner;
+
+public class ExceptionTest {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.print("두 정수를 입력하세요 : ");
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+		
+		// 조건문을 이용하여 예외가 발생하지 않도록 처리(예외 처리)
+		//2. 예외처리를 해주기
+		if(num !=0) {
+			int res = num1%num2;
+			System.out.println(num1 + "을 " + num2+ "로 나눈 나머지 : " + res);
+
+		}else {
+			System.out.println("0으로 나눌 수 없습니다.");
+		}
+	
+		//예외상황보려고 여기다가 2줄 추가한거임 => ??
+		int res = num1%num2;
+		System.out.println(num1 + "을 " + num2+ "로 나눈 나머지 : " + res);
+
+			//1. int res = num1 % num2; -> Scanner int num1,2 선언 이후 실행으로 num2에 0을 입력하면 예외가 발생하니 예외처리로 if문써서 "예외처리"했음
+
+			
+		scan.close();
+	}
+
+}
