ccbdb69d23d924202adc4c9833d5488fe0c144c9
moondiya717 <moondiya717@gmail.com>
2021-06-24 21:18:12 +0900

    JQuery 4일차: rolling예제(세로가로,방향바꿈)+rolling JS파일

---
+++ front_mhw/JS/JS/rolling.js
@@ -0,0 +1,23 @@
+function rollingTop(list, listItem, marginTop, animateTime, intervalTime){
+  var id = setInterval(function(){
+    if(!$(list + ' ' + listItem).first().is(':animated')){ //가아니면
+      $(list + ' ' + listItem).first()
+       .animate({'margin-top':marginTop+'px'},animateTime,function(){
+       $(this).detach().appendTo(list).removeAttr('style');
+      })
+    }
+  },intervalTime)
+  return id;
+}
+
+function rollingLeft(list, listItem, animateTime, intervalTime){
+  var id = setInterval(function(){
+    if($(list+ ' '+ listItem).first()){
+      var width = $(list+' '+listItem).first().width();
+      $(list+' '+listItem).first()
+        .animate({'margin-left':'-'+width}, animateTime, function(){
+        $(this).detach().appendTo(list).removeAttr('style');
+      })
+   }
+  },1500);
+}
