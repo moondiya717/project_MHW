817c84ef963815d5b6c33ea7eac2386a4cee9df3
moondiya717 <moondiya717@gmail.com>
2021-06-25 14:07:18 +0900

    JQuery 4일차: 롤링(rolling) js파일 기능 추가 및 수정

--- front_mhw/JS/JS/rolling.js
+++ front_mhw/JS/JS/rolling.js
@@ -12,12 +12,48 @@ function rollingTop(list, listItem, marginTop, animateTime, intervalTime){
 
 function rollingLeft(list, listItem, animateTime, intervalTime){
   var id = setInterval(function(){
-    if($(list+ ' '+ listItem).first()){
-      var width = $(list+' '+listItem).first().width();
+    if(!$(list+ ' '+ listItem).first().is(':animated')){
+      var width = $(list+' '+listItem).first().innerWidth();
       $(list+' '+listItem).first()
         .animate({'margin-left':'-'+width}, animateTime, function(){
         $(this).detach().appendTo(list).removeAttr('style');
       })
    }
-  },1500);
+  },intervalTime);
+  return id;
+}
+
+    
+function rollingRight(list, listItem, animateTime, intervalTime){
+  var id = setInterval(function(){
+  if(!$(list+ ' '+ listItem).first().is(':animated')){
+      var width = $(list+' '+listItem).last().innerWidth();
+      $(list+' '+listItem).last().detach().prependTo(list)
+      .css({'margin-left':-width+'px'}).animate({'margin-left':'0px'},animateTime);
+    } 
+  },intervalTime);
+  return id;
+}
+
+function rolling(deirection, list, listItem, margin, animateTime,intervalTime){
+  if(deirection.toLowerCase() == 'left'){
+    return rollingLeft(list, listItem, animateTime, intervalTime);
+  }
+  else if(deirection.toLowerCase()=='right'){
+    return rollingRight(list, listItem, animateTime, intervalTime);
+  }else{
+    return rollingTop(list, listItem, margin, animateTime, intervalTime);
+  }
+}
+
+function reorganizing_rollingToRight(list, listItem, animateTime, intervalTime){
+  var id=
+  setInterval(function(){
+    if(!$(list+ ' '+ listItem).first().is(':animated')){
+      var width= $(list+ ' '+ listItem).first().width();
+      $(list+ ' '+ listItem).first().detach().appendTo(list);
+      $(list).css('margin-left',-width+'px').animate({'margin-left':'0px'},animateTime)
+    }
+  },intervalTime);
+  return id;
 }
