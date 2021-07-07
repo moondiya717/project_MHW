3d0b347733ea4b6b732b0f3541dd38c0d828e762
moondiya717 <moondiya717@gmail.com>
2021-07-08 00:33:47 +0900

    SPRING test 서블릿에러나는 중

--- spring_home/src/main/java/kr/green/spring_home/HomeController.java
+++ spring_home/src/main/java/kr/green/spring_home/HomeController.java
@@ -1,39 +1,26 @@
 package kr.green.spring_home;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
+import kr.green.spring.service.MemberService;
+
+
 @Controller
 public class HomeController {
+    @Autowired
+    MemberService memberService;
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public ModelAndView home(ModelAndView mv) {		
+		mv.setViewName("home");
+
+		return mv;
 	}
+
+
 	
 }
