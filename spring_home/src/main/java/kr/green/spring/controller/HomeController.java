fffdfc5d9670bddafe5eb5e7a8d3c3e3363becc7
moondiya717 <moondiya717@gmail.com>
2021-07-08 09:19:22 +0900

    SPRING practice 서블릿에러 수정
    (controller이름변경+root.context.xml 코드추가)

--- spring_home/src/main/java/kr/green/spring/controller/HomeController.java
+++ spring_home/src/main/java/kr/green/spring/controller/HomeController.java
@@ -1,4 +1,4 @@
-package kr.green.spring_home;
+package kr.green.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
