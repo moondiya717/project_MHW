7e73c7a83bec3e75d17e7ce482ed3ba6b8149450
moondiya717 <moondiya717@gmail.com>
2021-07-12 13:00:09 +0900

    SPRING 6일차 : [spring_mhw] 비밀번호 암호화(로그인,회원가입)

--- spring_mhw/src/main/java/kr/green/spring/service/MemberServiceImp.java
+++ spring_mhw/src/main/java/kr/green/spring/service/MemberServiceImp.java
@@ -1,8 +1,9 @@
 package kr.green.spring.service;
  
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
- 
+
 import kr.green.spring.dao.MemberDAO;
 import kr.green.spring.vo.MemberVO;
  
@@ -10,6 +11,8 @@ import kr.green.spring.vo.MemberVO;
 public class MemberServiceImp implements MemberService {
     @Autowired
     MemberDAO memberDao;
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
     
 	@Override
 	public MemberVO signin(MemberVO user) {
@@ -21,8 +24,11 @@ public class MemberServiceImp implements MemberService {
 		MemberVO dbUser = memberDao.getMember(user.getId());
 		//가져온 회원 정보와 비밀번호를 확인하여 일치하면 회원정보를 반환하고
 		//일치하지 않으면 null을 반환
-		if(dbUser == null || !dbUser.getPw().equals(user.getPw())) { //나중에pw는 암호화처리해야해서 DB에서 직접 처리할 수 없음
-			return null;
+													//passwordEncoder.matches(A,B);
+													//A : 암호화 안된 문자열, B : 암호화된 문자열 => 같은 값인지 확인해줌
+		if(dbUser == null || passwordEncoder.matches(user.getPw(), dbUser.getPw())) { //나중에pw는 암호화처리해야해서 DB에서 직접 처리할 수 없음
+			return null; //비밀번호를 암호화해서 로그인하도록하면, 기존의 암호화안된 계정들은 로그인이 안되서
+						// => 연결되어있는 board글 작성자 아이디를 암호화된 계정 아이디로 다 바꾸고나서 암호화되지 않은 계정들을 삭제한다.
 		}
 		return dbUser;
 	}
@@ -33,6 +39,10 @@ public class MemberServiceImp implements MemberService {
 		if(user == null || memberDao.getMember(user.getId())!=null) { //memberDao.getMember DB에서 정보가져와서 확인해
 			return false;
 		}
+		//비밀번호 암호화
+		String encodePw = passwordEncoder.encode(user.getPw()); //위에 추가한 autowired에서 메소드 지원(내용입력하면 암호화해서 나옴)
+		user.setPw(encodePw);
+		
 		//없으면 다오에게 회원 정보를 주면서 회원가입 하라고 시킨 후, true를 리턴
 		memberDao.signup(user);
 		return true;
