107c8f226bb01c2f578cf19937bd573aac3ac38d
moondiya717 <moondiya717@gmail.com>
2021-07-13 17:47:37 +0900

    오타수정

--- spring_mhw/src/main/java/kr/green/spring/service/BoardService.java
+++ spring_mhw/src/main/java/kr/green/spring/service/BoardService.java
@@ -18,7 +18,7 @@ public interface BoardService {
 
 	int updateBoard(BoardVO board);
 	//void로 떴는데, 혹시라도 return값을 받아서 처리할 일이 생길 수도 있으니까 int로 변경
-	int deleteBoard(Integer numm, MemberVO user);
+	int deleteBoard(Integer num, MemberVO user);
 
 	int getTotalCount(Criteria cri); 
 
