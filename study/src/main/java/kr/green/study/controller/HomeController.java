959bd64f361ef6ea60ab7518c7e6184b875a80e4
moondiya717 <moondiya717@gmail.com>
2021-07-28 12:38:43 +0900

    SPRING18:[stduy] 연결확인화면출력

--- study/src/main/java/kr/green/study/controller/HomeController.java
+++ study/src/main/java/kr/green/study/controller/HomeController.java
@@ -1,14 +1,8 @@
 package kr.green.study.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import kr.green.study.service.MemberService;
 import lombok.AllArgsConstructor;
@@ -20,17 +14,12 @@ public class HomeController {
 
 	MemberService memberService;
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-	
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+	@GetMapping(value = "/")
+	public ModelAndView home(ModelAndView mv) {
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "/template/main/home";
+		mv.addObject("title","메인");
+		mv.setViewName("/template/main/home");
+		return mv;
 	}
 	
 }
--- study/src/main/java/kr/green/study/service/MemberServiceImp.java
+++ study/src/main/java/kr/green/study/service/MemberServiceImp.java
@@ -1,6 +1,5 @@
 package kr.green.study.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.green.study.dao.MemberDAO;
