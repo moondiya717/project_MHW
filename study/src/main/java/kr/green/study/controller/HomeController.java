21658432f9c2a30794b745f69aaaf9a489a00e87
moondiya717 <moondiya717@gmail.com>
2021-07-28 11:01:05 +0900

    SPRING18:[stduy] lombok추가 및 DB연동

--- study/src/main/java/kr/green/study/controller/HomeController.java
+++ study/src/main/java/kr/green/study/controller/HomeController.java
@@ -1,31 +1,28 @@
-package kr.green.study;
+package kr.green.study.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import kr.green.study.service.MemberService;
+import lombok.AllArgsConstructor;
+
+
 @Controller
+@AllArgsConstructor
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
+	MemberService memberService;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
+	
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
