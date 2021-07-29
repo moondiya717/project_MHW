9ab91cc2ce09842a1fd6c66b29027e6c1383ee83
moondiya717 <moondiya717@gmail.com>
2021-07-29 11:45:10 +0900

    SP19[study]권한별접근가능페이지제한인터셉터설정

---
+++ study/src/main/java/kr/green/study/interceptor/GuestInterceptor.java
@@ -0,0 +1,29 @@
+package kr.green.study.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import kr.green.study.service.MemberService;
+import kr.green.study.vo.MemberVO;
+
+public class GuestInterceptor extends HandlerInterceptorAdapter{
+
+	@Autowired
+	MemberService memberService;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+			Object handler) throws Exception{
+		HttpSession session = request.getSession();
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		if(user !=null) {
+			response.sendRedirect(request.getContextPath()+"/"); //메인으로 보냄
+			return false;
+		}
+		return true;
+	}
+}
